c0bdc392b34f4dffe97d5795a577774d450d8ef4
Tommy Nguyen <khenchaynguyen@gmail.com>
2024-08-09 01:27:38 +0700

    up city-tours-admin phase 11

--- city-tours-admin/src/services/region/RegionApi.js
+++ city-tours-admin/src/services/region/RegionApi.js
@@ -45,43 +45,6 @@ const regionApi = {
       return { error };
     }
   },
-
-  // createAccount: async (data) => {
-  //   const url = `${API_ROUTE.CREATE_USER}`;
-
-  //   try {
-  //     const response = await axiosClient.post(url, data);
-  //     return response;
-  //   } catch (error) {
-  //     return { error };
-  //   }
-  // },
-
-  // updateAccount: async (data) => {
-  //   const { userId, ...rest } = data;
-
-  //   const newData = { ...rest };
-
-  //   const url = `${API_ROUTE.UPDATE_USER}${userId}`;
-
-  //   try {
-  //     const response = await axiosClient.put(url, newData);
-  //     return response;
-  //   } catch (error) {
-  //     return { error };
-  //   }
-  // },
-
-  // deleteAccount: async (userId) => {
-  //   const url = `${API_ROUTE.UPDATE_USER}${userId}`;
-
-  //   try {
-  //     const response = await axiosClient.delete(url);
-  //     return response;
-  //   } catch (error) {
-  //     return { error };
-  //   }
-  // },
 };
 
 export default regionApi;
